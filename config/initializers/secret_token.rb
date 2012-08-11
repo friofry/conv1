5c268724b8d079337fbd58dd41dbb9f40d2e2d34
Andrey Bocharnikov <andrey.bocharnikov@gmail.com>
2012-08-11 13:58:29 +0700

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Conv1::Application.config.secret_token = '3969dc56b22c5447c80ea1083b3b8d9a2e5fd764e38fd0c37dc624f820c88d56138643bc9ab82af48d1ba3813d3ba1f82e4a43454c3bf0ac2dddedfaa861c9ee'
